f18c03afb4b2142d9a967c0997871d0cfc73c265
ilham <ilham.hafizh7@gmail.com>
2021-07-07 14:29:27 +0700

    Create Spalashscreen

--- app/src/main/java/com/d3if0028/katalogmov/SplashscreenActivity.kt
+++ app/src/main/java/com/d3if0028/katalogmov/SplashscreenActivity.kt
@@ -1,11 +1,18 @@
 package com.d3if0028.katalogmov
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashscreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
+        supportActionBar!!.hide()
+        Handler().postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        },3000)
     }
 }
