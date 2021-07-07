3ae02d0197bd12c14b76da02b7e965823baf09ef
ilham <ilham.hafizh7@gmail.com>
2021-07-07 14:10:40 +0700

    initial commit

---
+++ app/src/main/java/com/d3if0028/katalogmov/SplashscreenActivity.kt
@@ -0,0 +1,11 @@
+package com.d3if0028.katalogmov
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashscreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+    }
+}
