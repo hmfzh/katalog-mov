44505fb784928ab114f8ecefe82dd2feb0ea4642
ilham <ilham.hafizh7@gmail.com>
2021-08-07 17:44:44 +0700

    add infinite scroll fragment

--- app/src/main/java/com/d3if0028/katalogmov/fragment/NowPlayingFragment.kt
+++ app/src/main/java/com/d3if0028/katalogmov/fragment/NowPlayingFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.widget.NestedScrollView
 import androidx.recyclerview.widget.GridLayoutManager
 import com.d3if0028.katalogmov.R
 import com.d3if0028.katalogmov.activity.DetailActivity
@@ -15,6 +17,9 @@ import com.d3if0028.katalogmov.model.MovieModel
 import com.d3if0028.katalogmov.model.MovieResponse
 import com.d3if0028.katalogmov.retrofit.ApiService
 import kotlinx.android.synthetic.main.fragment_now_playing.view.*
+import kotlinx.android.synthetic.main.fragment_now_playing.view.list_movie
+import kotlinx.android.synthetic.main.fragment_now_playing.view.progress_movie
+import kotlinx.android.synthetic.main.fragment_now_playing.view.scroolView
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -23,6 +28,9 @@ import retrofit2.Response
 class NowPlayingFragment : Fragment() {
     lateinit var  v:View
     lateinit var mainAdapter : MainAdapter
+    private var isScrolling = false
+    private var currentPage = 1
+    private var totalPage = 0
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -34,11 +42,29 @@ class NowPlayingFragment : Fragment() {
     override fun onStart() {
         super.onStart()
         getMovieNowPlaying()
+        showLoadingNextPage(false)
     }
+
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setUpRecyleView()
+        setUpListener()
+    }
+
+    private fun setUpListener() {
+        v.scroolView.setOnScrollChangeListener(object: NestedScrollView.OnScrollChangeListener{
+            override fun onScrollChange(v: NestedScrollView?, scrollX: Int, scrollY: Int, oldScrollX: Int, oldScrollY: Int) {
+                if(scrollY == v!!.getChildAt(0).measuredHeight - v.measuredHeight){
+                    if(!isScrolling){
+                        if(currentPage <= totalPage ){
+                            getNowPlayingNextPage()
+                        }
+                    }
+                }
+            }
+        })
     }
 
     private fun setUpRecyleView() {
@@ -73,10 +99,43 @@ class NowPlayingFragment : Fragment() {
                 })
     }
 
+    fun getNowPlayingNextPage(){
+        currentPage += 1
+        showLoadingNextPage(true)
+        ApiService().endpoint.getMoviePopular(Constant.API_KEY,currentPage)
+                .enqueue(object : Callback<MovieResponse> {
+                    override fun onResponse(call: Call<MovieResponse>,
+                                            response: Response<MovieResponse>) {
+                        showLoadingNextPage(false)
+                        if(response.isSuccessful){
+                            showMovieNextPage(response.body()!!)
+                        }
+                    }
+
+                    override fun onFailure(call: Call<MovieResponse>, t: Throwable) {
+                        showLoadingNextPage(false)
+                    }
+
+                })
+    }
+
+
     fun showLoading(loading:Boolean){
         when(loading){
             true -> v.progress_movie.visibility = View.VISIBLE
             false -> v.progress_movie.visibility = View.GONE
+        }
+    }
+
+    fun showLoadingNextPage(loading:Boolean){
+        when(loading){
+            true -> {
+                isScrolling= true
+                v.progress_movie_next_page.visibility = View.VISIBLE}
+            false -> {
+                isScrolling = false
+                v.progress_movie_next_page.visibility = View.GONE
+            }
         }
     }
 
@@ -87,8 +146,19 @@ class NowPlayingFragment : Fragment() {
 //        for (movie in response.results){
 //            Log.d(Tag,"movie_title: ${movie.title}" )
 //        }
-
+        totalPage = response.total_pages!!
         mainAdapter.setData(response.results)
     }
 
+    fun showMovieNextPage(response: MovieResponse){
+//        Log.d(Tag,"responseMovie: $response")
+//        Log.d(Tag,"total_pages: ${response.total_pages}")
+//
+//        for (movie in response.results){
+//            Log.d(Tag,"movie_title: ${movie.title}" )
+//        }
+        totalPage = response.total_pages!!
+        mainAdapter.setDataNextPage(response.results)
+        Toast.makeText(requireContext(),"Page $currentPage", Toast.LENGTH_SHORT).show()
+    }
 }
--- app/src/main/java/com/d3if0028/katalogmov/fragment/PopularFragment.kt
+++ app/src/main/java/com/d3if0028/katalogmov/fragment/PopularFragment.kt
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.widget.NestedScrollView
 import androidx.recyclerview.widget.GridLayoutManager
 import com.d3if0028.katalogmov.R
 import com.d3if0028.katalogmov.activity.DetailActivity
@@ -27,6 +29,10 @@ class PopularFragment : Fragment() {
     lateinit var  v:View
     lateinit var mainAdapter : MainAdapter
 
+    private var isScrolling = false
+    private var currentPage = 1
+    private var totalPage = 0
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
@@ -37,11 +43,27 @@ class PopularFragment : Fragment() {
     override fun onStart() {
         super.onStart()
         getMoviePopular()
+        showLoadingNextPage(false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setUpRecyleView()
+        setUpListener()
+    }
+
+    private fun setUpListener() {
+        v.scroolView.setOnScrollChangeListener(object: NestedScrollView.OnScrollChangeListener{
+            override fun onScrollChange(v: NestedScrollView?, scrollX: Int, scrollY: Int, oldScrollX: Int, oldScrollY: Int) {
+                if(scrollY == v!!.getChildAt(0).measuredHeight - v.measuredHeight){
+                    if(!isScrolling){
+                        if(currentPage <= totalPage ){
+                            getMoviePopularNextPage()
+                        }
+                    }
+                }
+            }
+        })
     }
 
     private fun setUpRecyleView() {
@@ -76,10 +98,42 @@ class PopularFragment : Fragment() {
                 })
     }
 
+    fun getMoviePopularNextPage(){
+        currentPage += 1
+        showLoadingNextPage(true)
+        ApiService().endpoint.getMoviePopular(Constant.API_KEY,currentPage)
+                .enqueue(object : Callback<MovieResponse> {
+                    override fun onResponse(call: Call<MovieResponse>,
+                                            response: Response<MovieResponse>) {
+                        showLoadingNextPage(false)
+                        if(response.isSuccessful){
+                            showMovieNextPage(response.body()!!)
+                        }
+                    }
+
+                    override fun onFailure(call: Call<MovieResponse>, t: Throwable) {
+                        showLoadingNextPage(false)
+                    }
+
+                })
+    }
+
     fun showLoading(loading:Boolean){
         when(loading){
             true -> v.progress_movie.visibility = View.VISIBLE
             false -> v.progress_movie.visibility = View.GONE
+        }
+    }
+
+    fun showLoadingNextPage(loading:Boolean){
+        when(loading){
+            true -> {
+                isScrolling= true
+                v.progress_movie_next_page.visibility = View.VISIBLE}
+            false -> {
+                isScrolling = false
+                v.progress_movie_next_page.visibility = View.GONE
+            }
         }
     }
 
@@ -90,7 +144,19 @@ class PopularFragment : Fragment() {
 //        for (movie in response.results){
 //            Log.d(Tag,"movie_title: ${movie.title}" )
 //        }
-
+    totalPage = response.total_pages!!
         mainAdapter.setData(response.results)
+    }
+
+    fun showMovieNextPage(response: MovieResponse){
+//        Log.d(Tag,"responseMovie: $response")
+//        Log.d(Tag,"total_pages: ${response.total_pages}")
+//
+//        for (movie in response.results){
+//            Log.d(Tag,"movie_title: ${movie.title}" )
+//        }
+        totalPage = response.total_pages!!
+        mainAdapter.setDataNextPage(response.results)
+        Toast.makeText(requireContext(),"Page $currentPage",Toast.LENGTH_SHORT).show()
     }
 }
